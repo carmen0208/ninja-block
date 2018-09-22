4f96a6e3e59b10ef886cad0551ffd6021343145d
Carmen Liu <carmen.liujia@gmail.com>
2018-09-23 00:44:09 +1200

    make sure the chain is not change able

--- block.js
+++ block.js
@@ -1,12 +1,15 @@
 const SHA256 = require('crypto-js/sha256');
 class Block {
-  constructor(data, previousHash='') {
+  constructor(data, index=0, previousHash='') {
+    this.index = index
+    this.timestamp = new Date().toISOString().slice(0, 10);
     this.data = data
-    this.hash = this.calculateHash()
     this.previousHash = previousHash
+    this.hash = this.calculateHash()
   }
   calculateHash() {
-    return SHA256(this.previousHash + this.data).toString()
+    // console.log(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data))
+    return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
   }
 }
 
--- blockChain.js
+++ blockChain.js
@@ -2,10 +2,10 @@ const Block = require('./block')
 
 class BlockChain {
   constructor() {
-    this.chain = [this.createIintialBlock()]
+    this.chain = [this.createInitialBlock()]
   }
 
-  createIintialBlock() {
+  createInitialBlock() {
     return new Block('Initial Block', '0')
   }
 
@@ -14,8 +14,26 @@ class BlockChain {
   }
   addBlock(newBlock) {
     // console.log(this.getLastBlock())
+    newBlock.index = this.chain.length
     newBlock.previousHash = this.getLastBlock().hash
+    newBlock.hash = newBlock.calculateHash()
     this.chain.push(newBlock)
+  }
+
+  inChainValid() {
+    for(let i=1; i<this.chain.length; i++ ) {
+      const currentBlock = this.chain[i]
+      const previousBlock = this.chain[i - 1]
+      if(currentBlock.previousHash !== previousBlock.hash) {
+        return false
+      }
+      if(currentBlock.hash !== currentBlock.calculateHash()) {
+        // console.log(currentBlock.hash)
+        // console.log(currentBlock.calculateHash())
+        return false
+      }
+    }
+    return true
   }
 }
 
