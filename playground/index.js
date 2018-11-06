d2bc8c605bf2596694f32e456f648dc53a8b55e3
Carmen Liu <carmen.liujia@gmail.com>
2018-11-06 21:37:42 +1300

    clean up the branch to be just blockchain explaination

--- playground/index.js
+++ playground/index.js
@@ -1,35 +1,8 @@
 const Block = require("../lib/blockchain/block");
 const BlockChain = require("../lib/blockchain/blockChain");
-const Transaction = require("../lib/blockchain/transaction");
-// let ninjaBlock = new Block({amount:100})
-// let ninjaBlock2 = new Block({amount: 500}, ninjaBlock.hash)
-// console.log(ninjaBlock)
-// console.log(ninjaBlock2)
 
 let ninjaBlockChain = new BlockChain();
-// ninjaBlockChain.addBlock(new Block({amount:100}))
-// ninjaBlockChain.addBlock(new Block({amount:200}))
-// console.log(ninjaBlockChain)
-// console.log(`if blockchain is still valid: ${ninjaBlockChain.inChainValid()}`)
-
-// console.log('Warning: Blockchain has been changed')
-// ninjaBlockChain.chain[1].data ={amout:500}
-// console.log(`if blockchain is still valid: ${ninjaBlockChain.inChainValid()}`)
-
-ninjaBlockChain.createTransaction(new Transaction("carmen", "nico", "520"));
-ninjaBlockChain.createTransaction(new Transaction("nico", "carmen", "1314"));
-
-ninjaBlockChain.miningTransaction("miner-reward-address");
-console.log("-------------- mining -------------------");
-console.log(ninjaBlockChain);
-console.log(ninjaBlockChain.getBalanceFromAddress("miner-reward-address"));
-
-console.log("-------------- mining again -------------------");
-ninjaBlockChain.miningTransaction("miner-reward-address");
-console.log(ninjaBlockChain.getBalanceFromAddress("miner-reward-address"));
-
-const Wallet = require('../lib/operator/wallet')
-const wallet = new Wallet('Carmen')
-console.log(wallet)
-wallet.generateAddress()
-wallet.generateAddress()
+ninjaBlockChain.addBlock(new Block({amount:100}))
+ninjaBlockChain.addBlock(new Block({amount:200}))
+console.log(ninjaBlockChain)
+console.log(`if blockchain is still valid: ${ninjaBlockChain.inChainValid()}`)
