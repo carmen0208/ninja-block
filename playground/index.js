dcb21d56b4af0c825748ba6f4e560561b367757f
Carmen Liu <carmen.liujia@gmail.com>
2018-11-11 11:50:53 +1300

    format

--- playground/index.js
+++ playground/index.js
@@ -1,8 +1,8 @@
-const Block = require("../lib/blockchain/block");
-const BlockChain = require("../lib/blockchain/blockChain");
+const Block = require('../lib/blockchain/block')
+const BlockChain = require('../lib/blockchain/blockChain')
 
-let ninjaBlockChain = new BlockChain();
-ninjaBlockChain.addBlock(new Block({amount:100}))
-ninjaBlockChain.addBlock(new Block({amount:200}))
+let ninjaBlockChain = new BlockChain()
+ninjaBlockChain.addBlock(new Block({amount: 100}))
+ninjaBlockChain.addBlock(new Block({amount: 200}))
 console.log(ninjaBlockChain)
 console.log(`if blockchain is still valid: ${ninjaBlockChain.inChainValid()}`)
